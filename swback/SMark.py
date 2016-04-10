18bcb26b0838e44ec4dedc8032aad4fe9bfd7cd9
dinosaurav <sauravd@gmail.com>
2016-04-10 02:50:55 -0400

    initial working commit, added synonym search

--- swback/SMark.py
+++ swback/SMark.py
@@ -1,11 +1,13 @@
 import nltk as nl
 import random as rnd
 import string
+import getSynonyms as wordnet
 
 class SynonymWatermark:
     def __init__(self, txt, complexity):
         self.text = txt
         self.complexity = complexity
+        self.net = wordnet.loadWordNet()
 
     def notProperWord(self, word):
         return word[0].islower()
@@ -17,6 +19,16 @@ class SynonymWatermark:
             copies.append(makeCopy())
 
         return copies
+    
+    def convertPOS(self,pos):
+        if "N" in pos:
+            return 'n'
+        if "V" in pos:
+            return 'v'
+        if "A" in pos:
+            return 'a'
+        print pos
+        return 'n'
 
     def makeCopy(self):
         tokens = nl.word_tokenize(self.text)
@@ -28,8 +40,11 @@ class SynonymWatermark:
             weight = weight + rnd.random() * self.complexity
 
             if weight > 1 and self.notProperWord(tokens[i]):
-                synonym = "" #send tags[i] to thesaurus
-                tokens[i] = synonym
-                weight = 0
+                _,pos = tags[i]
+                pos = self.convertPOS(pos)
+                synonym = wordnet.getSynonym(self.net,tokens[i],pos) #send tags[i] to thesaurus
+                if synonym:
+                    tokens[i] = synonym
+                    weight = 0
 
         return "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
---
+++ swback/getSynonyms.py
@@ -0,0 +1,50 @@
+# load wordnet file
+# parse wordnet file
+import re
+
+filename = "core-wordnet.txt"
+
+def loadWordNet():
+  #read file
+  f = open(filename,'r')
+
+  wordnet = {}
+  wordnet["a"] = {}
+  wordnet["n"] = {}
+  wordnet["v"] = {}
+
+
+  for line in f:
+    #format
+    #(pos) [(word)%#:##:##::] [(word_again)] syn,syn,syn
+    matchObj = re.match( r'([anv]) \[(.*)%.*\] \[(.*)\](.*)',line)#\[(.*)\] (?:([^,]), )*',line)
+    pos = matchObj.group(1)
+    word = matchObj.group(2)
+    word2 = matchObj.group(3)
+    syn = matchObj.group(4)
+    syn = syn.strip().split(", ")
+    
+    if not(word in wordnet[pos]):
+      wordnet[pos][word] = []
+    wordnet[pos][word].extend(syn)
+      
+  f.close()
+  return wordnet
+
+
+def getSynonym(net,word,pos):
+  print word
+  print pos
+  if not(word in net[pos]):
+    return None
+  for syn in net[pos][word]:
+    if len(syn) > 0:
+      return syn
+  return None
+  #pick the most common of the words
+
+net = loadWordNet()
+print getSynonym(net,"love","a")
+print getSynonym(net,"black","a")
+print getSynonym(net,"arbitrary","a")
+print getSynonym(net,"aware","a")
